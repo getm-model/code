5abb6c2cf2a02da7b2672e76c04c980e1d3ba1dc
gotm <gotm>
2003-03-24 12:48:21 +0000

    Fixed one loop bdy + cleaned up

--- src/3d/slow_advection.F90
+++ src/3d/slow_advection.F90
@@ -1,4 +1,4 @@
-!$Id: slow_advection.F90,v 1.1 2002-05-02 14:00:54 gotm Exp $
+!$Id: slow_advection.F90,v 1.2 2003-03-24 12:48:21 gotm Exp $
 #include "cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -31,8 +31,11 @@
 !  Original author(s): Hans Burchard & Karsten Bolding
 !
 !  $Log: slow_advection.F90,v $
-!  Revision 1.1  2002-05-02 14:00:54  gotm
-!  Initial revision
+!  Revision 1.2  2003-03-24 12:48:21  gotm
+!  Fixed one loop bdy + cleaned up
+!
+!  Revision 1.1.1.1  2002/05/02 14:00:54  gotm
+!  recovering after CVS crash
 !
 !  Revision 1.6  2001/08/27 11:50:17  bbh
 !  TVD-advection for momentum added, some bugs removed
@@ -85,98 +88,91 @@
    end do
 
 ! Upstream for dx(U^2/D)
-
    do j=jjmin,jjmax
       do i=iimin,iimax+1         ! PP defined on T-points
          if (az(i,j).ge.1) then
             PP(i,j)=0.5*(Uint(i-1,j)+Uint(i,j))
             if (PP(i,j) .gt. _ZERO_ ) then
-	       ii=i-1
-	    else
-	       ii=i
-	    end if
-	    PP(i,j)=PP(i,j)*Uint(ii,j)/DUi(ii,j)*DYC
-	 end if
+               ii=i-1
+            else
+               ii=i
+            end if
+            PP(i,j)=PP(i,j)*Uint(ii,j)/DUi(ii,j)*DYC
+         end if
       end do
    end do
-
    do j=jjmin,jjmax
       do i=iimin,iimax           ! UEx defined on U-points
          if (au(i,j).ge.1) then
-         UEx(i,j)=(PP(i+1,j)-PP(i  ,j))*ARUD1
+            UEx(i,j)=(PP(i+1,j)-PP(i  ,j))*ARUD1
 	 end if
       end do
    end do
 
 ! Upstream for dy(UV/D)
-
    do i=iimin-1,iimax
       do j=jjmin-1,jjmax     ! PP defined on X-points
          if ((au(i,j).ge.1).or.(au(i,j+1).ge.1)) then
             PP(i,j)=0.5*(Vint(i+1,j)+Vint(i,j))
-	    if (PP(i,j) .gt. _ZERO_) then
-	       jj=j
-	    else
-	       jj=j+1
-	    end if
-	    PP(i,j)=PP(i,j)*Uint(i,jj)/DUi(i,jj)*DXX
-	 end if
+            if (PP(i,j) .gt. _ZERO_) then
+               jj=j
+            else
+               jj=j+1
+            end if
+            PP(i,j)=PP(i,j)*Uint(i,jj)/DUi(i,jj)*DXX
+         end if
       end do
    end do
-
    do j=jjmin,jjmax
       do i=iimin,iimax       !UEx defined on U-points
          if (au(i,j).ge.1) then
-         UEx(i,j)=UEx(i,j)+(PP(i,j  )-PP(i,j-1))*ARUD1
-	 end if
+            UEx(i,j)=UEx(i,j)+(PP(i,j  )-PP(i,j-1))*ARUD1
+         end if
       end do
    end do
 
 ! Upstream for dx(UV/D)
-
    do j=jjmin-1,jjmax
       do i=iimin-1,iimax      ! PP defined on X-points
          if ((av(i,j).ge.1).or.(av(i+1,j).ge.1)) then
             PP(i,j)=0.5*(Uint(i,j)+Uint(i,j+1))
-	    if (PP(i,j) .gt. _ZERO_) then
-	       ii=i
-	    else
-	       ii=i+1
-	    end if
-	    PP(i,j)=PP(i,j)*Vint(ii,j)/DVi(ii,j)*DYX
-	 end if
+            if (PP(i,j) .gt. _ZERO_) then
+               ii=i
+            else
+               ii=i+1
+            end if
+            PP(i,j)=PP(i,j)*Vint(ii,j)/DVi(ii,j)*DYX
+         end if
       end do
    end do
-
    do j=jjmin,jjmax
       do i=iimin,iimax       ! VEx defined on V-points
          if (av(i,j).ge.1) then
-         VEx(i,j)=(PP(i  ,j)-PP(i-1,j))*ARVD1
-	 end if
+            VEx(i,j)=(PP(i  ,j)-PP(i-1,j))*ARVD1
+         end if
       end do
    end do
 
 ! Upstream for dy(V^2/D)
-
    do j=jjmin,jjmax+1
-      do i=iimin,iimax+1         ! PP defined on T-points
+      do i=iimin,iimax         ! PP defined on T-points
+         PP(i,j) = _ZERO_
          if (az(i,j).ge.1) then
-         PP(i,j)=0.5*(Vint(i,j-1)+Vint(i,j))
-	 if (PP(i,j) .gt. _ZERO_) then
-	    jj=j-1
-	 else
-	    jj=j
-	 end if
-	 PP(i,j)=PP(i,j)*Vint(i,jj)/DVi(i,jj)*DXC
-	 end if
+            PP(i,j)=0.5*(Vint(i,j-1)+Vint(i,j))
+            if (PP(i,j) .gt. _ZERO_) then
+               jj=j-1
+            else
+               jj=j
+            end if
+            PP(i,j)=PP(i,j)*Vint(i,jj)/DVi(i,jj)*DXC
+         end if
       end do
    end do
-
    do j=jjmin,jjmax
       do i=iimin,iimax           ! VEx defined on V-points
          if (av(i,j).ge.1) then
-         VEx(i,j)=VEx(i,j)+(PP(i,j+1)-PP(i,j  ))*ARVD1
-	 end if
+            VEx(i,j)=VEx(i,j)+(PP(i,j+1)-PP(i,j  ))*ARVD1
+         end if
       end do
    end do
 
