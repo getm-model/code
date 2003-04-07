35c22a253127df52ffce1acf217df3de19f02883
kbk <kbk>
2003-04-07 15:50:20 +0000

    initialise variables

--- src/2d/variables_2d.F90
+++ src/2d/variables_2d.F90
@@ -1,4 +1,4 @@
-!$Id: variables_2d.F90,v 1.2 2002-05-29 13:37:49 gotm Exp $
+!$Id: variables_2d.F90,v 1.3 2003-04-07 15:50:20 kbk Exp $
 #include "cppdefs.h"
 !-----------------------------------------------------------------------
 !BOP
@@ -15,7 +15,7 @@
 !  The module contains public subroutines to initialise and cleanup.
 !
 ! !USES:
-   use domain,     only: imin,imax,jmin,jmax,H,HU,HV,min_depth
+   use domain, only: imin,imax,jmin,jmax,H,HU,HV,min_depth
    IMPLICIT NONE
 !
 ! !PUBLIC DATA MEMBERS:
@@ -31,15 +31,14 @@
 !  Original author(s): Karsten Bolding & Hans Burchard
 !
 !  $Log: variables_2d.F90,v $
-!  Revision 1.2  2002-05-29 13:37:49  gotm
-!  New naming of .h files
+!  Revision 1.3  2003-04-07 15:50:20  kbk
+!  initialise variables
 !
 !  Revision 1.1.1.1  2002/05/02 14:00:47  gotm
 !  recovering after CVS crash
 !
 !  Revision 1.1  2001/05/03 19:30:41  bbh
 !  2D variables seperated from m2d
-!
 !
 ! !LOCAL VARIABLES:
    integer	:: rc
@@ -91,6 +90,16 @@
 #ifndef STATIC
 #include "dynamic_allocations_2d.h"
 #endif
+
+   z = _ZERO_ ; zu = _ZERO_ ; zv = _ZERO_
+   U = _ZERO_ ; DU = _ZERO_ ; fU = _ZERO_ ; SlUx = _ZERO_ ; Slru = _ZERO_
+   V = _ZERO_ ; DV = _ZERO_ ; fV = _ZERO_ ; SlVx = _ZERO_ ; Slrv = _ZERO_
+
+   Uint = _ZERO_ ; Vint = _ZERO_
+   UEx = _ZERO_ ; VEx = _ZERO_
+   ru = _ZERO_ ; rv = _ZERO_
+   res_du = _ZERO_ ; res_u =  _ZERO_ ; res_dv = _ZERO_ ; res_v =  _ZERO_
+   surfdiv = _ZERO_
 
 #ifdef DEBUG
    write(debug,*) 'Leaving init_variables_2d()'
