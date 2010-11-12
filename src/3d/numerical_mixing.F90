aebb3bb45b0ce98f29f2b008446b0acb49bf726d
Karsten Bolding <karsten@bolding-burchard.com>
2010-11-12 14:05:32 +0100

    added numerical/physical mixing analysis

---
+++ src/3d/numerical_mixing.F90
@@ -0,0 +1,49 @@
+#include "cppdefs.h"
+!-----------------------------------------------------------------------
+!BOP
+!
+! !ROUTINE: numerical_mixing() 
+!
+! !INTERFACE:
+   subroutine numerical_mixing(F_2,F,nm3D,nm2d)
+!
+! !DESCRIPTION:
+!
+!
+! !USES:
+   use domain,       only: imin,imax,jmin,jmax,kmax
+   use variables_3d, only: dt,hn
+   IMPLICIT NONE
+!
+! !INPUT PARAMETERS:
+   REALTYPE, intent(in)  :: F_2(I3DFIELD)
+   REALTYPE, intent(in)  :: F(I3DFIELD)
+!
+! !OUTPUT PARAMETERS:
+   REALTYPE, intent(out) :: nm3d(I3DFIELD)
+   REALTYPE, intent(out) :: nm2d(I2DFIELD)
+!
+! !REVISION HISTORY:
+!  Original author(s): Hannes Rennau 
+!
+! !LOCAL VARIABLES:
+   integer                   :: i,j,k
+!EOP
+!-----------------------------------------------------------------------
+!BOC
+   do k=1,kmax 
+      do j=jmin,jmax
+         nm2d(i,j)=_ZERO_
+         do i=imin,imax
+            nm3d(i,j,k)=(F_2(i,j,k)-F(i,j,k)**2)/dt
+            nm2d(i,j)=nm2d(i,j)+nm3d(i,j,k)*hn(i,j,k)
+         end do
+      end do
+   end do
+   return
+   end subroutine numerical_mixing
+!BOC
+
+!-----------------------------------------------------------------------
+! Copyright (C) 2010 - Hannes Rennau, Richard Hofmeister               !
+!-----------------------------------------------------------------------
---
+++ src/3d/physical_mixing.F90
@@ -0,0 +1,59 @@
+#include "cppdefs.h"
+!-----------------------------------------------------------------------
+!BOP
+!
+! !ROUTINE: physical_mixing() 
+!
+! !INTERFACE:
+   subroutine physical_mixing(F,diffusivity,pm3d,pm2d)
+!
+! !DESCRIPTION:
+!
+
+! !USES:
+   use domain,       only: imin,imax,jmin,jmax,kmax,H
+   use variables_3d, only: dt,nuh,hn,ssen
+   IMPLICIT NONE
+
+! !INPUT PARAMETERS:
+   REALTYPE, intent(in)  :: F(I3DFIELD)
+   REALTYPE, intent(in)  :: diffusivity(I3DFIELD)
+!
+! !INPUT PARAMETERS
+   REALTYPE, intent(out) :: pm3d(I3DFIELD)
+   REALTYPE, intent(out) :: pm2d(I2DFIELD)
+!
+! !REVISION HISTORY:
+!  Original author(s): Hannes Rennau 
+!
+! !LOCAL VARIABLES:
+   REALTYPE                  :: dupper,dlower
+   integer                   :: i,j,k 
+!EOP
+!-----------------------------------------------------------------------
+!BOC
+   do j=jmin,jmax
+      do i=imin,imax
+         pm2d(i,j)=_ZERO_
+         dlower=_ZERO_
+         do k=1,kmax-1
+           if (k.eq.kmax) then
+             dupper=_ZERO_
+           else
+             dupper=_TWO_*(diffusivity(i,j,k))*(F(i,j,k+1)-F(i,j,k))**2 &
+                      /(_HALF_*(hn(i,j,k+1)+hn(i,j,k)))**2
+           end if
+           pm3d(i,j,k)=_HALF_*(dupper+dlower)
+           dlower=dupper
+           pm2d(i,j)=pm2d(i,j)+pm3d(i,j,k)*hn(i,j,k)
+         end do
+      end do
+   end do 
+   return
+   end subroutine physical_mixing
+!EOC
+
+!-----------------------------------------------------------------------
+! Copyright (C) 2010 - Hannes Rennau, Richard Hofmeister               !
+!-----------------------------------------------------------------------
+
