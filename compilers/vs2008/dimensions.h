d4ac3de99cdaea6e6874d2b70c5d97f80ca821a7
Jorn Bruggeman <jorn@bolding-burchard.com>
2011-08-23 11:28:22 +0100

    switched to relative MS-MPI path via include; removed case-specific dimensions.h

--- compilers/vs2008/dimensions.h
+++
@@ -1,4 +0,0 @@
-   integer, parameter :: iextr=100,jextr=30
-   integer, parameter :: imin=1,imax=iextr,jmin=1,jmax=jextr
-   integer, parameter :: iimin=1,iimax=iextr,jjmin=1,jjmax=jextr
-   integer, parameter :: kmax=30
