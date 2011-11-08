f0d2ad8814524e23122f1164b531a01373125d46
Knut <knutaros@gmx.net>
2011-11-08 16:48:05 +0100

    limited range for check of numbers of vertical iterations

--- src/3d/adv_w_split_3d.F90
+++ src/3d/adv_w_split_3d.F90
@@ -120,8 +120,11 @@
                end do
                iters = max(1,ceiling(cfl))
                if (iters .gt. itersmax_adv) then
-                  FATAL 'adv_w_split_3d: too many iterations needed at'
-                  FATAL 'i=',i,' j=',j,':',iters
+                  if (      imin-HALO.ne.i .and. i.ne.imax+HALO &
+                      .and. jmin-HALO.ne.j .and. j.ne.jmax+HALO ) then
+                     FATAL 'adv_w_split_3d: too many iterations needed at'
+                     FATAL 'i=',i,' j=',j,':',iters
+                  end if
                end if
 #ifdef DEBUG
                if (iters .gt. 1) write(95,*) i,j,iters,cfl
