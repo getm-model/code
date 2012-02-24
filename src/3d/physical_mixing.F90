bd0610a790a35b15973a1e22a98cc832ac59a1db
Knut <knutaros@gmx.net>
2012-02-24 01:07:39 +0100

    bugfix physical_mixing analysis for AH_method=0

--- src/3d/physical_mixing.F90
+++ src/3d/physical_mixing.F90
@@ -5,7 +5,7 @@
 ! !ROUTINE: physical_mixing()
 !
 ! !INTERFACE:
-   subroutine physical_mixing(F,diffusivity,pm3d,pm2d)
+   subroutine physical_mixing(F,diffusivity,pm3d,pm2d,AH_method)
 !
 ! !DESCRIPTION:
 !
@@ -22,14 +22,15 @@
 !
 !
 ! !USES:
-   use domain,       only: imin,imax,jmin,jmax,kmax,H
-   use variables_3d, only: dt,nuh,hn,ssen
+   use domain,       only: imin,imax,jmin,jmax,kmax,az
+   use variables_3d, only: dt,nuh,hn
 
    IMPLICIT NONE
 
 ! !INPUT PARAMETERS:
    REALTYPE, intent(in)  :: F(I3DFIELD)
    REALTYPE, intent(in)  :: diffusivity
+   integer, intent(in)   :: AH_method
 !
 ! !OUTPUT PARAMETERS
    REALTYPE, intent(out) :: pm3d(I3DFIELD)
@@ -48,19 +49,24 @@
 
    do j=jmin,jmax
       do i=imin,imax
-         pm2d(i,j)=_ZERO_
-         dlower=_ZERO_
-         do k=1,kmax
-            if (k .eq. kmax) then
-               dupper=_ZERO_
-            else
-               dupper=_TWO_*(diffusivity+nuh(i,j,k))*(F(i,j,k+1)-F(i,j,k))**2 &
-                      /(_HALF_*(hn(i,j,k+1)+hn(i,j,k)))**2
+         if (az(i,j) .eq. 1) then
+            if (AH_method .eq. 0) then
+               pm3d(i,j,:) = _ZERO_
             end if
-            pm3d(i,j,k) = pm3d(i,j,k) + _HALF_*(dupper+dlower)
-            dlower=dupper
-            pm2d(i,j)=pm2d(i,j)+pm3d(i,j,k)*hn(i,j,k)
-         end do
+            pm2d(i,j)=_ZERO_
+            dlower=_ZERO_
+            do k=1,kmax
+               if (k .eq. kmax) then
+                  dupper=_ZERO_
+               else
+                  dupper=_TWO_*(diffusivity+nuh(i,j,k))*(F(i,j,k+1)-F(i,j,k))**2 &
+                         /(_HALF_*(hn(i,j,k+1)+hn(i,j,k)))**2
+               end if
+               pm3d(i,j,k) = pm3d(i,j,k) + _HALF_*(dupper+dlower)
+               dlower=dupper
+               pm2d(i,j)=pm2d(i,j)+pm3d(i,j,k)*hn(i,j,k)
+            end do
+         end if
       end do
    end do
 
