5f2e92e8ecffd19fd96914e35d852b64a1d22fc5
Karsten Bolding <karsten@bolding-burchard.com>
2012-04-27 15:46:59 +0200

    with rudimentary error handling - fortran_version.sh

--- fortran_version.sh
+++ fortran_version.sh
@@ -4,10 +4,15 @@ if [ "x$FORTRAN_COMPILER" = "xIFORT" ] ; then
    ifort --version 2>&1 > ifort.tmp
    echo  "#define FORTRAN_VERSION \"`head -1 ifort.tmp`\""
    rm -f ifort.tmp
+   return 0
 fi
 
 if [ "x$FORTRAN_COMPILER" = "xGFORTRAN" ] ; then 
   gfortran --version 2>&1 >  gfortran.tmp
   echo  "#define FORTRAN_VERSION \"gfortran `head -1 gfortran.tmp`\""
   rm gfortran.tmp
+  return 0
 fi
+
+echo "FORTRAN_COMPILER must be set"
+return 1
