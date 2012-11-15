31ddac6e6b9ff3e58b2493203b6f96f1b8653904
Knut <knutaros@gmx.net>
2012-11-15 18:49:36 +0100

    changes to preadapt

--- src/3d/preadapt_coordinates.F90
+++ src/3d/preadapt_coordinates.F90
@@ -18,8 +18,8 @@
    use getm_timers, only: tic, toc, TIM_COORDS
 #ifndef NO_BAROCLINIC
    use m3d, only: calc_salt, calc_temp
-   use salinity, only: init_salinity_field, do_salinity
-   use temperature, only: init_temperature_field, do_temperature
+   use salinity, only: init_salinity_field,
+   use temperature, only: init_temperature_field
    use eqstate, only: do_eqstate
    use internal_pressure, only: do_internal_pressure
 #endif
@@ -43,30 +43,22 @@
    if (preadapt.ne.0) then
       LEVEL1 'pre-adapting coordinates'
       do ii=1,preadapt
-         call start_macro()
          call adaptive_coordinates(.false.,.false.)
-         call ww_momentum_3d()
 #ifndef NO_BAROCLINIC
-         if(calc_salt) call do_salinity(1)
-         if(calc_temp) call do_temperature(1)
+         if(calc_salt) then
+            LEVEL2 'reinterpolating initial salinity'
+            call init_salinity_field()
+         end if
+         if(calc_temp) then
+            LEVEL2 'reinterpolating initial temperature'
+            call init_temperature_field()
+         end if
          call do_eqstate()
          call buoyancy_frequency()
 #endif
-         call stop_macro()
-         if (mod(ii,10).eq._ZERO_) LEVEL3 ii
       end do
 
 #ifndef NO_BAROCLINIC
-      LEVEL2 'reinterpolating initial salinity'
-      if(calc_salt) then
-         call init_salinity_field()
-      end if
-      LEVEL2 'reinterpolating initial temperature'
-      if(calc_temp) then
-         call init_temperature_field()
-      end if
-      call do_eqstate()
-      call buoyancy_frequency()
       call do_internal_pressure()
 #endif
    end if
