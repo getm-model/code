da956b92975f29887b953e618056b30b484e0053
Knut <knutaros@gmx.net>
2014-01-05 04:25:31 +0100

    bugfix: dynamic compilation requires target attributes for some arrays

--- src/2d/dynamic_declarations_2d.h
+++ src/2d/dynamic_declarations_2d.h
@@ -3,10 +3,10 @@
   integer,dimension(:,:),allocatable   :: break_stat
 #endif
   REALTYPE,dimension(:,:),allocatable,target :: t_zo,t_z,D,Dvel,DU,DV
-  REALTYPE,dimension(:,:),allocatable  :: U,V
+  REALTYPE,dimension(:,:),allocatable,target :: U,V
   REALTYPE,dimension(:,:),allocatable  :: UEx,VEx
   REALTYPE,dimension(:,:),allocatable  :: ru,rv
-  REALTYPE,dimension(:,:),allocatable  :: Uint,Vint
+  REALTYPE,dimension(:,:),allocatable,target :: Uint,Vint
   REALTYPE,dimension(:,:),allocatable  :: res_du,res_u
   REALTYPE,dimension(:,:),allocatable  :: res_dv,res_v
   REALTYPE,dimension(:,:),allocatable  :: SlUx,SlVx
