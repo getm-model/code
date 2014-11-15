ff1f568ad8eaecfbe4585b9760f9a8277dc2b412
Knut <knutaros@gmx.net>
2014-11-15 22:46:11 +0100

    bioshade: bugfix: allocate bioshade independent on calc_fabm

--- src/3d/variables_3d.F90
+++ src/3d/variables_3d.F90
@@ -179,7 +179,6 @@
 #ifdef _FABM_
    REALTYPE, allocatable, dimension(:,:,:,:) :: fabm_pel,fabm_diag
    REALTYPE, allocatable, dimension(:,:,:)   :: fabm_ben,fabm_diag_hz
-   REALTYPE, allocatable, dimension(:,:,:), target :: bioshade
 #endif
    integer                             :: size3d_field
    integer                             :: mem3d
