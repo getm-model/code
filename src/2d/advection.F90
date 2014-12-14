79c1a9c91c046a084215e2e3a88584020390213f
Knut <knutaros@gmx.net>
2014-12-14 21:54:42 +0100

    adv: _NEW_ADV_NOSPLIT_ is now default

--- src/2d/advection.F90
+++ src/2d/advection.F90
@@ -509,19 +509,7 @@
                                    p_nvd)
 #endif
 
-#ifdef _NEW_ADV_NOSPLIT_
-!                 Note (KK): causes truncation errors
                   f = fi
-#else
-                  do j=jmin-HALO,jmax+HALO
-                     do i=imin-HALO,imax+HALO
-                        if (adv_grid%mask_finalise(i,j)) then
-!                          Note (KK): do not modify tracer inside open bdy cells
-                           f(i,j) = ( Do(i,j)*f(i,j) - dt*p_adv(i,j) ) / p_Di(i,j)
-                        end if
-                     end do
-                  end do
-#endif
 
                case(UPSTREAM_2DH)
 
