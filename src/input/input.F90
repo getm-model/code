e421b4b924bffd3870aa89be4fcc6a0cc7941229
Knut <knutaros@gmx.net>
2015-01-07 13:54:28 +0100

    INPUT_DIR: do not prepend INPUT_DIR to meteo_file

--- src/input/input.F90
+++ src/input/input.F90
@@ -105,7 +105,7 @@
 
    LEVEL1 'init_input'
    if (met_method .eq. METEO_FROMFILE) then
-      call init_meteo_input(trim(input_dir) // meteo_file,n)
+      call init_meteo_input(meteo_file,n)
    end if
 
    if (waveforcing_method .eq. WAVES_FROMFILE) then
