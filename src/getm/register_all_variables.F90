aae42b6ffd0fdfde61ff42ba4367b0510d3c3f5a
Knut <knutaros@gmx.net>
2015-11-04 17:19:40 +0100

    output: consider grid_type and provide non-empty longnames for coordinate dimensions

--- src/getm/register_all_variables.F90
+++ src/getm/register_all_variables.F90
@@ -33,20 +33,12 @@
 !
 ! !LOCAL VARIABLES:
    integer,parameter :: rk = kind(_ONE_)
-#if defined SPHERICAL
-   character(len=16)         :: xname='lonc'
-   character(len=16)         :: xlongname='longitude'
-   character(len=16)         :: xunits='degrees_east'
-   character(len=16)         :: yname='latc'
-   character(len=16)         :: ylongname='latitude'
-   character(len=16)         :: yunits='degrees_north'
-#elif defined CURVILINEAR
-   character(len=16)         :: xname='xic'
-   character(len=16)         :: yname='etac'
-#else
-   character(len=16)         :: xname='xc'
-   character(len=16)         :: yname='yc'
-#endif
+   character(len=16)         :: xname=''
+   character(len=16)         :: xlongname=''
+   character(len=16)         :: xunits=''
+   character(len=16)         :: yname=''
+   character(len=16)         :: ylongname=''
+   character(len=16)         :: yunits=''
    character(len=16)         :: zname='sigma'
    character(len=64)         :: zlongname='sigma'
    character(len=16)         :: zunits='sigma'
@@ -55,6 +47,28 @@
 !BOC
 
    LEVEL1 'register_all_variables()'
+
+   select case (grid_type)
+      case (1)
+         xname     = 'xc'
+         xlongname = 'x'
+         xunits    = 'm'
+         yname     = 'yc'
+         ylongname = 'y'
+         yunits    = 'm'
+      case (2)
+         xname     = 'lonc'
+         xlongname = 'longitude'
+         xunits    = 'degrees_east'
+         yname     = 'latc'
+         ylongname = 'latitude'
+         yunits    = 'degrees_north'
+      case (3)
+         xname     = 'xic'
+         xlongname = 'xic'
+         yname     = 'etac'
+         ylongname = 'etac'
+   end select
 
 #ifndef NO_3D
    if (runtype .ge. 2) then
