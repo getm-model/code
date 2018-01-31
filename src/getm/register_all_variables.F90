3de4fda934f88f5a32c41b3505e7e64daf47fde8
Knut <knutaros@gmx.net>
2018-01-31 12:13:11 +0100

    Sflux: correct units

--- src/getm/register_all_variables.F90
+++ src/getm/register_all_variables.F90
@@ -589,8 +589,8 @@
 #endif
 
    if (update_salt) then
-      call fm%register('Sfluxu', 'g/kg*m2/s', 'salt flux in local x-direction', standard_name='', dimensions=(/id_dim_z/), category='3d', output_level=output_level_debug, used=save_Sfluxu)
-      call fm%register('Sfluxv', 'g/kg*m2/s', 'salt flux in local y-direction', standard_name='', dimensions=(/id_dim_z/), category='3d', output_level=output_level_debug, used=save_Sfluxv)
+      call fm%register('Sfluxu', 'g/kg*m3/s', 'salt flux in local x-direction', standard_name='', dimensions=(/id_dim_z/), category='3d', output_level=output_level_debug, used=save_Sfluxu)
+      call fm%register('Sfluxv', 'g/kg*m3/s', 'salt flux in local y-direction', standard_name='', dimensions=(/id_dim_z/), category='3d', output_level=output_level_debug, used=save_Sfluxv)
       call fm%register('Sfluxw', 'g/kg*m/s', 'vertical salt flux'            , standard_name='', dimensions=(/id_dim_z/), category='3d', output_level=output_level_debug, used=save_Sfluxw)
    end if
 
