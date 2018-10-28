ac404d183b5e27c00b76da260ab1133c63f7f46d
Knut <knutaros@gmx.net>
2018-10-28 21:48:09 +0100

    fwf_int: correct branch name

--- src/futils/getm_version.F90
+++ src/futils/getm_version.F90
@@ -1,4 +1,4 @@
 module getm_version
    character(len=*),parameter :: git_commit_id = "2.5.0"
-   character(len=*),parameter :: git_branch_name = "output"
+   character(len=*),parameter :: git_branch_name = "fwf_int"
 end module
