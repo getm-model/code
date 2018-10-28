210281f6392213452fed52339d6dad3a6794ebe2
Knut <knutaros@gmx.net>
2018-10-28 22:29:20 +0100

    STDERR_DIR: correct branch name

--- src/futils/getm_version.F90
+++ src/futils/getm_version.F90
@@ -1,4 +1,4 @@
 module getm_version
    character(len=*),parameter :: git_commit_id = "2.5.0"
-   character(len=*),parameter :: git_branch_name = "slice_model"
+   character(len=*),parameter :: git_branch_name = "STDERR_DIR"
 end module
