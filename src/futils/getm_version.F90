192171cd2ea146ab76ca698dfc8529715ba82c19
Knut <knutaros@gmx.net>
2018-10-28 22:20:55 +0100

    daf: correct branch name

--- src/futils/getm_version.F90
+++ src/futils/getm_version.F90
@@ -1,4 +1,4 @@
 module getm_version
    character(len=*),parameter :: git_commit_id = "2.5.0"
-   character(len=*),parameter :: git_branch_name = "waves"
+   character(len=*),parameter :: git_branch_name = "daf"
 end module
