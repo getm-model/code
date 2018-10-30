b66d6c0a4407d7b732401fe7cf6b7f5575e74134
Knut <knutaros@gmx.net>
2018-10-30 23:21:11 +0100

    REAL_4B: correct branch name

--- src/futils/getm_version.F90
+++ src/futils/getm_version.F90
@@ -1,4 +1,4 @@
 module getm_version
    character(len=*),parameter :: git_commit_id = "2.5.0"
-   character(len=*),parameter :: git_branch_name = "INPUT_DIR"
+   character(len=*),parameter :: git_branch_name = "REAL_4B"
 end module
