02812dcbb784c2821b0e516b91e4e10fcf734468
Karsten Bolding <karsten@bolding-bruggeman.com>
2019-06-06 13:10:25 +0200

    added version info of NetCDF and MPI?

--- src/getm/print_version.F90
+++ src/getm/print_version.F90
@@ -21,16 +21,25 @@
    use fabm_version, only: fabm_commit_id=>git_commit_id, &
                            fabm_branch_name=>git_branch_name
 #endif
+   use netcdf
    IMPLICIT NONE
+   integer :: v, sv
+   character(len=8) :: ver
 !
 !EOP
 !-----------------------------------------------------------------------
 !BOC
    LEVEL0 LINE
-   LEVEL0 'GETM version: ',getm_commit_id,' (',getm_branch_name,' branch)'
-   LEVEL0 'GOTM version: ',gotm_commit_id,' (',gotm_branch_name,' branch)'
+   LEVEL0 'GETM:   ',getm_commit_id,' (',getm_branch_name,' branch)'
+   LEVEL0 'GOTM:   ',gotm_commit_id,' (',gotm_branch_name,' branch)'
 #ifdef _FABM_
-   LEVEL0 'FABM version: ',fabm_commit_id,' (',fabm_branch_name,' branch)'
+   LEVEL0 'FABM:   ',fabm_commit_id,' (',fabm_branch_name,' branch)'
+#endif
+   LEVEL0 'NetCDF: ',trim(NF90_INQ_LIBVERS())
+#ifdef GETM_PARALLEL
+   call MPI_get_version(v,sv)
+   write(ver,'(i1,a1,i1)') v,'.',sv
+   LEVEL0 'MPI:    ',trim(ver)
 #endif
    LEVEL0 LINE
    LEVEL0 'Compiler: ',compiler_id,' ',compiler_version
